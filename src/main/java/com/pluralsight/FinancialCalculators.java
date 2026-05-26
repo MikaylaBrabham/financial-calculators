e7b88f0c012aa1a9ea0ba51875a80fea96fbebc9
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-25 23:01:36 -0500

    added financial calculators and psuedo plan

--- src/main/java/com/pluralsight/FinancialCalculators.java
+++ src/main/java/com/pluralsight/FinancialCalculators.java
@@ -1,4 +1,11 @@
 package com.pluralsight;
-
+//add imports
 public class FinancialCalculators {
+
+    //add scanners
+
+    //add main
+
+    //add menu to pick which calculator you'd want to use
+
 }
